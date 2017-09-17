81bd0c008f0c2444ffb5ccef6616be6be21d46ed
Sergey <bugakov.sa.88@gmail.com>
2017-09-17 19:39:12 +0300

    fill run_test.py

--- run_test.py
+++ run_test.py
@@ -1,3 +1,29 @@
 import urllib.request
-with urllib.request.urlopen("http://localhost:8080/state") as f:
-    print(f.read().decode('utf-8'))
+import time
+
+url = "http://localhost:8080/"
+
+def executeRequest(url):
+    with urllib.request.urlopen(url) as response:
+        return response.read().decode('utf-8')
+
+def executeTests():
+
+    if executeRequest(url + "start") == "started":
+        print("Tester started")
+    else:
+        print("Tester not started")
+        return False
+
+    while executeRequest(url + "state") != "finish":
+        time.sleep(2)
+
+    if executeRequest(url + "success") == "true":
+        print("0 errors")
+        return True
+    else:
+        print(executeRequest(url + "report"))
+        return False
+
+if not executeTests():
+    raise Exception('Error during testing. See log for details.')
